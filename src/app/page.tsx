6a41efbe6ce489a1720d598c778f70834ae20423
Elian Paredes <elianparedes.dev@gmail.com>
2023-06-18 23:02:24 -0300

    feat: page layout with simple rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+"use client";
+
+import { Editor } from "@monaco-editor/react";
+import { useEffect, useRef, useState } from "react";
+import createCompilerModule from "../compiler/Compiler";
+import BubblegumLogo from "@/components/logo";
+import { PlayIcon } from "lucide-react";
+
+const editorOptions = {
+    fontSize: 13,
+};
+
+function wrapParse(Module: any) {
+    return function (code: string, callback: Function): void {
+        let codePtr = Module.stringToNewUTF8(code);
+
+        // Call the C function using ccall
+        let result = Module.ccall("getCode", "number", ["number"], [codePtr]);
+        let parsedCode = Module.UTF8ToString(result);
+
+        Module._free(result);
+        Module._free(codePtr);
+
+        callback(parsedCode);
+    };
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [value, setValue] = useState<string | undefined>(
+        "Rectangle(width: 300, height: 300, fill-color: #00ff00)"
+    );
+    const [parse, setParse] = useState<Function>();
+
+    const [code, setCode] = useState("");
+
+    useEffect(() => {
+        createCompilerModule().then((Module: any) => {
+            setParse(() => wrapParse(Module));
+        });
+        console.log("adg");
+    }, [code]);
+
+    function handleParse() {
+        if (!parse) {
+            return;
+        }
+
+        parse(value, (code: string) => setCode(code));
+    }
+
+    function handleEditorChange(value: string | undefined, ev: any) {
+        setValue(value);
+    }
+
+    useEffect(() => {
+        console.log(code);
+    });
+
+    if (!parse) return <div>Loading environment...</div>;
+
+    return (
+        <div className="grid grid-cols-[1fr_100vh] grid-rows-[48px_1fr] h-[100vh] bg-white dark:bg-neutral-900">
+            <header
+                data-tauri-drag-region
+                className="col-start-1 col-end-3 row-start-1 row-end-2 border-b dark:border-neutral-800 border-neutral-200 flex justify-between items-center pl-7 pr-2"
+            >
+                <BubblegumLogo className="hover:scale-125 duration-150" />
+                <div
+                    onClick={handleParse}
+                    className="h-full px-4 bg-neutral-900 hover:bg-[#D73FB9] duration-100 border border-neutral-800 flex items-center cursor-pointer"
+                >
+                    <PlayIcon size={16} />
+                </div>
+            </header>
+
+            <section className="col-start-1 col-end-2 row-start-2 row-end-3 border-r dark:border-neutral-800 border-neutral-200 flex flex-col overflow-y-hidden ">
+                <Editor
+                    defaultLanguage="dart"
+                    value={value}
+                    theme="vs-dark"
+                    options={editorOptions}
+                    onChange={handleEditorChange}
+                />
+            </section>
+
+            <main className="flex-grow overflow-y-auto transition-colors duration-300">
+                <iframe
+                    srcDoc={code.replace(/(\r\n|\n|\r|\t)/gm, "")}
+                    className="w-full h-full"
+                />
+            </main>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
---
+++ src/components/logo.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function BubblegumLogo({className}: any) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            width="24"
+            height="24"
+            viewBox="0 0 300 300"
+        >
+            <defs>
+                <radialGradient
+                    id="radial-gradient"
+                    cx="0.688"
+                    cy="0.274"
+                    r="0.505"
+                    gradientUnits="objectBoundingBox"
+                >
+                    <stop offset="0" stop-color="#f3dcff" />
+                    <stop offset="0.544" stop-color="#db41be" />
+                    <stop offset="1" stop-color="#9d2b7a" />
+                </radialGradient>
+            </defs>
+            <circle
+                id="Ellipse_3"
+                data-name="Ellipse 3"
+                cx="150"
+                cy="150"
+                r="150"
+                fill="url(#radial-gradient)"
+            />
+        </svg>
+    );
+}
